90513c053f6f44fdad5aea22e4948781bef03aa1
Dennis Nienhuis <dbdenny1@hotmail.com>
2017-03-08 20:25:11 +0100

    Added base for notification

---
+++ notification-example/notifications_client.lua
@@ -0,0 +1,19 @@
+addEventHandler("onClientResourceStart", resourceRoot, function()
+	call ( getResourceFromName ( "notification" ), "addNotification", "THIS IS A INFO MESSAGE ON RIGHT TOP", "info", 5000, "right-top" );
+	exports.notification:addNotification("THIS IS A INFO MESSAGE ON RIGHT TOP", "info", 5000, "right-top");
+	exports['notification']:addNotification("THIS IS A INFO MESSAGE ON RIGHT TOP", "info", 5000, "right-top");
+	
+	exports['notification']:addNotification("THIS IS A INFO MESSAGE ON RIGHT TOP", "info", 5000, "right-top")
+	exports['notification']:addNotification("THIS IS A WARNING MESSAGE ON TOP", "warning", 5000, "top")
+	exports['notification']:addNotification("THIS IS A NEWS MESSAGE ON LEFT TOP", "news", 5000, "left-top")
+
+	exports['notification']:addNotification("THIS IS A INFO MESSAGE ON LEFT TOP", "info", 5000, "left-top")
+	exports['notification']:addNotification("THIS IS A WARNING MESSAGE ON RIGHT TOP", "warning", 5000, "right-top")
+	exports['notification']:addNotification("THIS IS A NEWS MESSAGE ON TOP", "news", 5000, "top")
+
+
+	exports['notification']:addNotification("THIS IS A INFO MESSAGE ON TOP", "info", 5000, "top")
+	exports['notification']:addNotification("THIS IS A WARNING MESSAGE ON LEFT TOP", "warning", 5000, "left-top")
+	exports['notification']:addNotification("THIS IS A NEWS MESSAGE ON RIGHT TOP", "news", 5000, "right-top")
+	outputChatBox("test");
+end)
